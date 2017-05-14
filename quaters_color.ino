6540ca5f608d3c4c5b48d29959d5e04bdd3ce7c5
bgonen22 <bgonen22@gmail.com>
2017-05-14 22:52:04 +0300

    initail

---
+++ quaters_color.ino
@@ -0,0 +1,98 @@
+#include <Adafruit_NeoPixel.h>
+// Which pin on the Arduino is connected to the NeoPixels?
+#define PIN            6
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS      60
+
+// the last led of each quarter (the 4th is NUMOFPIXELS-1)
+int q1_end = 19;
+int q2_end = 39;
+int q3_end = 59;
+
+#define NUMOFCOLORS  30  
+// the color jump of each iteration
+#define COLOR_JUMP  4
+
+// delay between iterations
+#define DELAY 10000 // delay between colors change (msec)
+#define FADE 5 // delay fade (msec)
+
+// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
+// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
+// example for more information on possible values.
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  Serial.begin(9600);
+  pixels.begin(); // This initializes the NeoPixel library.
+
+}
+
+void loop() {
+  int c1 = 1;
+  int c2 = 50;
+  int c3 = 100;
+  int c4 = 150;
+  while (1) {
+    light_trace(c1,c2,c3,c4,1); // fade_int    
+    delay (DELAY);
+    light_trace(c1,c2,c3,c4,0); // fade_out    
+    c1 = (c1 + COLOR_JUMP)%NUMOFCOLORS;
+    c2 = (c2 + COLOR_JUMP)%NUMOFCOLORS;
+    c3 = (c3 + COLOR_JUMP)%NUMOFCOLORS;
+    c4 = (c4 + COLOR_JUMP)%NUMOFCOLORS;    
+  }
+}
+
+void light_trace(int c1, int c2, int c3, int c4,int fade_in) {
+  float power = 0;
+  for (int i = 0 ; i< 10; ++i) {
+    if (fade_in) {
+      power = i/10.0;
+    } else {
+      power = 1 - i/10.0;
+    }
+//    Serial.print(power);
+    //Serial.print(" ");
+    //Serial.print(i);
+    //Serial.print(" ");
+    //Serial.println(fade_in);
+    light_quarter (c1,0,q1_end,power);
+    light_quarter (c2,q1_end+1,q2_end,power);
+    light_quarter (c3,q2_end+1,q3_end,power);
+    light_quarter (c4,q3_end+1,NUMPIXELS-1,power);
+    pixels.show(); // This sends the updated pixel color to the hardware.    
+    delay (FADE);
+  }
+}
+
+void light_quarter (int color, int start_led, int end_led,float power) {        
+  for (int i = start_led; i< end_led; ++i) {
+    pixels.setPixelColor(i, Wheel(color,power));       
+  }  
+  
+}
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+// the level is how brigt will be tghe light (0 to 255).
+uint32_t Wheel(byte color, float level) {
+
+  if (level==0) return pixels.Color(0, 0, 0);
+  //level=100;   
+  float power;
+ // Serial.println(color);
+  if(color < NUMOFCOLORS/3) {
+     power=1.0*color/(NUMOFCOLORS/3)*255;
+     return pixels.Color(level*(255 - power), 0, level*power); 
+  } else if(color < 2*NUMOFCOLORS/3) {
+      color -= NUMOFCOLORS/3;
+      power=1.0*color/(NUMOFCOLORS/3)*255;
+      return pixels.Color(0, level*power, level*(255 - power));
+  } else {
+     color -= 2*NUMOFCOLORS/3;
+     power=1.0*color/(NUMOFCOLORS/3)*255;
+     return pixels.Color(level*power, level*(255 - power), 0);
+  }
+}
+
